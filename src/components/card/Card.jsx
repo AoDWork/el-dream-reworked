e3d4c9f9e9a759ff31494e99f71a7634772194a8
AoDWork <aodtest07@gmail.com>
2022-08-28 17:20:22 +0300

    Pre Hooks

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,15 @@
+import s from "./Card.module.css"
+
+const Card = (props) => {
+    return(
+        <div className={s.card}>
+            Hello World
+            {/*<a href="/female" className={s.cardLink}>*/}
+            {/*    <h3 className={s.cardTitle}>Жіноче</h3>*/}
+            {/*    <img className={s.cardImg} src={Female} alt="Жіноче"/>*/}
+            {/*</a>*/}
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/resources/db/cards.js
@@ -0,0 +1,8 @@
+const CARDS = {
+  male: {link: "/male", cardName: "Чоловіче", image: "../../resources/img/main/category_male.jpg" },
+  female: {link: "/female", cardName: "Жіноче", image: "../../resources/img/main/category_female.jpg" },
+  childrens: {link: "/female", cardName: "Дитяче", image: "../../resources/img/main/category_childrens.jpg" },
+  plaid: {link: "/plaid", cardName: "Пледи", image: "../../resources/img/main/category_plaid.jpg" }
+}
+
+export default CARDS;
