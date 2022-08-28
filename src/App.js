347592353144c9569a4fe03f336504af8f86820a
AoDWork <aodtest07@gmail.com>
2022-08-28 21:59:30 +0300

    Whithout shit in index.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,16 @@ import Header from "./components/header/Header";
 import Content from "./components/content/Content";
 import Female from './components/content/female/Female'
 import Footer from "./components/footer/Footer";
-import {BrowserRouter as Router, Routes, Switch, Route, Link, useRouteMatch, useParams} from "react-router-dom";
 
 function App() {
   return (
-      <Router>
-          <div className="App">
-              <div className='Header'><Header/></div>
-              <div className='Content'>
-                  <Routes>
-                      <Route path="/" element={<Content />} />
-                      <Route path="/female" element={<Female />} />
-                  </Routes>
-              </div>
-              <div className='Footer'><Footer/></div>
-          </div>
-      </Router>
+    <div className="App">
+      <div className='Header'><Header /></div>
+      <div className='Content'>
+        <Content />
+      </div>
+      <div className='Footer'><Footer /></div>
+    </div>
   );
 }
 
