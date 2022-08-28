a0d71630a303f5d2be1603c61c67ac99b88235ed
AoDWork <aodtest07@gmail.com>
2022-08-28 17:31:58 +0300

    Pre hooks

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,7 +3,7 @@ import s from "./Card.module.css"
 const Card = (props) => {
     return(
         <div className={s.card}>
-            Hello World
+            "Hello World"
             {/*<a href="/female" className={s.cardLink}>*/}
             {/*    <h3 className={s.cardTitle}>Жіноче</h3>*/}
             {/*    <img className={s.cardImg} src={Female} alt="Жіноче"/>*/}
