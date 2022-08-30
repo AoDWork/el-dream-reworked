5bb8ce0b9e3ce4feca5b3d102a2ef7cb34ccdb4d
AoDWork <aodtest07@gmail.com>
2022-08-30 09:02:57 +0300

    some error stayed

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -8,12 +8,13 @@ import Card from '../card/Card'
 
 
 const Content = () => {
+
     const [cards, setCards] = useState([
         {male: {link: "/male", cardName: "Чоловіче", image: "../../resources/img/main/category_male.jpg" }},
         {female: {link: "/female", cardName: "Жіноче", image: "../../resources/img/main/category_female.jpg" }},
         {childrens: {link: "/female", cardName: "Дитяче", image: "../../resources/img/main/category_childrens.jpg" }},
         {plaid: {link: "/plaid", cardName: "Пледи", image: "../../resources/img/main/category_plaid.jpg" }}
-]);
+    ]);
 
 
     function renderCards() {
